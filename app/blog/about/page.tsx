8686a14e133cb260319a18e524b0c05a30f57a4c
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-06-06 23:45:46 +0800

    INITIAL COMMIT

---
+++ app/blog/about/page.tsx
@@ -0,0 +1,14 @@
+export default function About() {
+	return (
+		<div className="mt-3">
+			<h1 className="font-bold text-2xl lg:text-3xl">About</h1>
+
+			<p className="max-w-[1050px] mt-3">
+				Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quisquam
+				labore debitis natus expedita iure aliquam distinctio architecto porro
+				obcaecati, delectus, voluptatibus iusto laboriosam dolorum
+				exercitationem reiciendis? Natus dolorem aliquid tempore!
+			</p>
+		</div>
+	);
+}
---
+++ components/app-sidebar.tsx
@@ -0,0 +1,62 @@
+"use client";
+
+import {
+	Sidebar,
+	SidebarContent,
+	SidebarGroup,
+	SidebarGroupContent,
+	SidebarGroupLabel,
+	SidebarMenu,
+	SidebarMenuButton,
+	SidebarMenuItem,
+} from "@/components/ui/sidebar";
+import clsx from "clsx";
+import { Home, InfoIcon } from "lucide-react";
+import { usePathname } from "next/navigation";
+
+const items = [
+	{
+		title: "My Blogs",
+		url: "/blog",
+		icon: Home,
+	},
+	{
+		title: "About",
+		url: "/blog/about",
+		icon: InfoIcon,
+	},
+];
+
+export function AppSidebar() {
+	const pathname = usePathname();
+
+	return (
+		<Sidebar>
+			<SidebarContent>
+				<SidebarGroup>
+					<SidebarGroupLabel>Welcome to Blog Site</SidebarGroupLabel>
+					<SidebarGroupContent>
+						<SidebarMenu>
+							{items.map((item) => (
+								<SidebarMenuItem key={item.title}>
+									<SidebarMenuButton asChild>
+										<a
+											href={item.url}
+											className={clsx(
+												pathname === item.url &&
+													"bg-slate-900 text-white hover:bg-slate-900 hover:text-white"
+											)}
+										>
+											<item.icon />
+											<span>{item.title}</span>
+										</a>
+									</SidebarMenuButton>
+								</SidebarMenuItem>
+							))}
+						</SidebarMenu>
+					</SidebarGroupContent>
+				</SidebarGroup>
+			</SidebarContent>
+		</Sidebar>
+	);
+}
