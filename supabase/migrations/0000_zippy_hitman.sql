bd01e5fdc2dbf46fd1b546fb9e31347c4e44b476
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-06-07 01:32:49 +0800

    setup drizzle

---
+++ supabase/migrations/0000_zippy_hitman.sql
@@ -0,0 +1,18 @@
+CREATE TABLE "posts_table" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"title" text NOT NULL,
+	"content" text NOT NULL,
+	"user_id" integer NOT NULL,
+	"created_at" timestamp DEFAULT now() NOT NULL,
+	"updated_at" timestamp NOT NULL
+);
+--> statement-breakpoint
+CREATE TABLE "users" (
+	"id" serial PRIMARY KEY NOT NULL,
+	"name" text NOT NULL,
+	"email" text NOT NULL,
+	"password" text NOT NULL,
+	CONSTRAINT "users_email_unique" UNIQUE("email")
+);
+--> statement-breakpoint
+ALTER TABLE "posts_table" ADD CONSTRAINT "posts_table_user_id_users_id_fk" FOREIGN KEY ("user_id") REFERENCES "public"."users"("id") ON DELETE cascade ON UPDATE no action;
