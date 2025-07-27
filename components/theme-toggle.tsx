795721789d852db184d2f38717837954db4fb50a
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-07-27 22:20:09 +0800

    add checking in theme mode button for hydration

--- components/theme-toggle.tsx
+++ components/theme-toggle.tsx
@@ -1,12 +1,19 @@
 "use client";
 
+import { Button } from "@/components/ui/button";
 import { Moon, Sun } from "lucide-react";
 import { useTheme } from "next-themes";
-
-import { Button } from "@/components/ui/button";
+import { useEffect, useState } from "react";
 
 export function ModeToggle() {
 	const { setTheme, theme } = useTheme();
+	const [mounted, setMounted] = useState(false);
+
+	useEffect(() => {
+		setMounted(true);
+	}, []);
+
+	if (!mounted) return null; // Avoid hydration mismatch
 
 	return (
 		<Button
@@ -19,7 +26,7 @@ export function ModeToggle() {
 			{theme === "light" ? (
 				<Sun className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all dark:scale-0 dark:-rotate-90" />
 			) : (
-				<Moon className="absolute h-[1.2rem] w-[1.2rem] scale-0 rotate-90 transition-all dark:scale-100 dark:rotate-0" />
+				<Moon className="h-[1.2rem] w-[1.2rem] scale-100 rotate-0 transition-all" />
 			)}
 		</Button>
 	);
