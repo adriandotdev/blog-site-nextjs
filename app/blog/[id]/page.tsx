d829cb42a7a11b84d73bf12e19d5984ad27af466
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-07-28 00:12:36 +0800

    add UI fallback when blog is private

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { getBlogById } from "@/app/actions";
 import { SimpleEditor } from "@/components/tiptap-templates/simple/simple-editor";
+import Link from "next/link";
 
 type Params = Promise<{ id: string }>;
 
@@ -7,6 +8,25 @@ export default async function BlogPage({ params }: { params: Params }) {
 	const { id } = await params;
 
 	const blog = await getBlogById(+id);
+
+	if (blog.visibility === "private") {
+		return (
+			<div className="flex flex-col justify-center items-center  min-h-[50vh]">
+				<h1 className="text-2xl md:text-4xl lg:text-5xl font-archivo font-bold">
+					This content is private.
+				</h1>
+				<p className="text-md md:text-xl">
+					Only the author can view this content.
+				</p>
+				<Link
+					href="/blogs"
+					className="mt-3 font-bold border-[0.5px] bg-slate-950 text-white px-5 py-3 rounded-[8px]"
+				>
+					Go back
+				</Link>
+			</div>
+		);
+	}
 
 	return (
 		<div className="mx-3 mt-3">
