2e0eccaa1e3668aca63976ccd973c8edc4945d2c
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-08-17 16:14:35 +0800

    update landing page
    change width of navbar and footer

--- components/home/navbar.tsx
+++ components/home/navbar.tsx
@@ -6,26 +6,28 @@ export default function Navbar() {
 	const router = useRouter();
 
 	return (
-		<div className="py-5 border-b flex justify-between lg:px-24 items-center xl:px-24">
-			<div>
-				<h1 className="font-bold font-archivo">
-					The
-					<span className="bg-gradient-to-b from-orange-800  via-red-600  to-orange-400 bg-clip-text text-transparent">
-						Daily
-					</span>
-					Bytes
-				</h1>
-			</div>
-			<div className="flex flex-row gap-3">
-				<ModeToggle />
-				<Button
-					onClick={() => {
-						router.push("/signin");
-					}}
-					className="hover:cursor-pointer bg-gradient-to-l from-orange-800  via-red-600  to-orange-400 text-white font-bold"
-				>
-					Try it for free
-				</Button>
+		<div className="w-full">
+			<div className="py-5  flex justify-between items-center max-w-[1148px] mx-auto px-8">
+				<div>
+					<h1 className="font-bold font-archivo">
+						The
+						<span className="bg-gradient-to-b from-orange-800  via-red-600  to-orange-400 bg-clip-text text-transparent">
+							Daily
+						</span>
+						Bytes
+					</h1>
+				</div>
+				<div className="flex flex-row gap-3">
+					<ModeToggle />
+					<Button
+						onClick={() => {
+							router.push("/signin");
+						}}
+						className="hover:cursor-pointer bg-gradient-to-l from-orange-800  via-red-600  to-orange-400 text-white font-bold"
+					>
+						Try it for free
+					</Button>
+				</div>
 			</div>
 		</div>
 	);
